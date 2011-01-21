707fe5b81bd7e9f41d66681cb076cdd2ff52ee5b
Jesse Luehrs <doy@tozt.net>
2011-01-21 13:46:09 -0600

    stop using $Bread::Board::CC

--- lib/OX/Role/Object.pm
+++ lib/OX/Role/Object.pm
@@ -10,22 +10,24 @@ sub BUILD {
     container $self => as {
         if ($meta->has_components) {
             container Component => as {
-                $Bread::Board::CC->add_service($_) for $meta->components;
+                my $c = shift;
+                $c->add_service($_) for $meta->components;
             };
         }
 
         if ($meta->has_config) {
             container Config => as {
-                $Bread::Board::CC->add_service($_) for $meta->config;
+                my $c = shift;
+                $c->add_service($_) for $meta->config;
             };
         }
 
         if ($meta->has_router_config) {
-            $Bread::Board::CC->add_service($meta->router_config);
+            $self->add_service($meta->router_config);
         }
 
         if ($meta->has_router) {
-            $Bread::Board::CC->add_service(
+            $self->add_service(
                 Bread::Board::Literal->new(
                     name  => 'Router',
                     value => $meta->router,
