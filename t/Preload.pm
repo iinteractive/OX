13082d663082e78d58b7a4f55832ab054b554275
Jesse Luehrs <doy@tozt.net>
2012-11-12 16:40:26 -0600

    add module preloader for the benefit of forkprove

---
+++ t/Preload.pm
@@ -0,0 +1,12 @@
+package t::Preload;
+use strict;
+use warnings;
+
+use OX;
+use OX::Request;
+use OX::Response;
+use OX::RouteBuilder::Code;
+use OX::RouteBuilder::ControllerAction;
+use OX::RouteBuilder::HTTPMethod;
+
+1;
