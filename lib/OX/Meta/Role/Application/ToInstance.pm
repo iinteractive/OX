a06080713c10ed1a77b6270f5b1e391c6178b927
Jesse Luehrs <doy@tozt.net>
2012-11-13 13:15:25 -0600

    actually, ToClass already handles this

--- lib/OX/Meta/Role/Application/ToInstance.pm
+++ lib/OX/Meta/Role/Application/ToInstance.pm
@@ -2,25 +2,11 @@ package OX::Meta::Role::Application::ToInstance;
 use Moose::Role;
 use namespace::autoclean;
 
-use Moose::Util 'find_meta';
-
 with 'OX::Meta::Role::Application';
 
 sub _apply_routes {
     my $self = shift;
     my ($role, $obj) = @_;
-
-    my $class = find_meta($obj);
-
-    for my $route ($role->routes) {
-        $class->_add_route($route)
-            unless $class->has_route_for($route->path);
-    }
-
-    for my $mount ($role->mounts) {
-        $class->_add_mount($mount)
-            unless $class->has_mount_for($mount->path);
-    }
 
     $obj->regenerate_router_config;
 }
