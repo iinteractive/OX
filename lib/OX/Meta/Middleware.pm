494cd947246680b5be42ffef99cdfa434aac63b2
Jesse Luehrs <doy@tozt.net>
2013-08-05 16:41:38 -0400

    move prepare_app call to just after instantiation (fixes gh-13)
    previously it was being called as part of to_app, which is called every
    time the middleware is applied. now it is only called every time the
    middleware is instantiated (which makes a difference for singleton
    middleware).

--- lib/OX/Meta/Middleware.pm
+++ lib/OX/Meta/Middleware.pm
@@ -42,9 +42,28 @@ has service => (
         );
         my $mw_service;
         if (!ref($mw)) {
-            $mw_service = Bread::Board::ConstructorInjection->new(
+            $mw_service = Bread::Board::BlockInjection->new(
                 %common,
-                class        => $mw,
+                block => sub {
+                    my $s = shift;
+
+                    my $mw_obj = $mw->new(
+                        map { $_ => $s->param($_) } $s->get_param_keys
+                    );
+
+                    # this should just be sub { $mw_obj->wrap($_[0]) }
+                    # (actually, this whole thing should just be a constructor
+                    # injection) but wrap calls to_app, which calls prepare_app
+                    # every time, which we'd like to avoid for singleton
+                    # middleware. it is kind of gross though, would be nice to
+                    # maybe push this back into plack
+                    $mw_obj->prepare_app;
+                    return sub {
+                        my $app = shift;
+                        $mw_obj->{app} = $app;
+                        sub { $mw_obj->call(@_) };
+                    };
+                },
                 dependencies => $deps,
             );
         }
